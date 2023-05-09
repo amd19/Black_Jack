916ed02cf955c76023a23467a571012c35c6774a
amd19 <98783909+amd19@users.noreply.github.com>
2023-05-09 17:22:00 +0000

    Adding rules and fixed placement of the fucntions

--- MainClass.java
+++ MainClass.java
@@ -4,27 +4,52 @@ import java.util.LinkedList; // will be used ot make delaers hand, player's had 
 
 public class MainClass { 
     public static boolean keep_playing = true; // boolean for while-loop
-    public static int wallet = 2500; // players inital amount of money to bet 
+    public static int wallet = 500; // players inital amount of money to bet 
     LinkedList hand = new LinkedList<Card>();
     LinkedList splitHand = new LinkedList<Card>();
     LinkedList dealerHand = new LinkedList<Card>();
     LinkedList deck = new LinkedList<Card>();
+    public static int playerCounter = 0, dealerCounter = 0, pot =0; // hold value of how many cards have been dealt to player, pot holds betting value
+    
     public static void main(String[] args) {
         
         // will be used for testing as of 03-09-23
+        int score = 0;
+        int gamesWon = 0;
         Deck gameDeck = new Deck(52);
         gameDeck.generateDeckInfo();
-        gameDeck.printDeckInfo();
+        printMenu(score, gamesWon);
     }
-}
-public void printMenu(){
-    // will pront out game menu
-}
-public void hit(){
-    // add card to players hand
-}
-public void split(){
-    // will split current players hand
+
+    // functions 
+    public static void printRules(){
+        System.out.println("\tRules for Black Jack \n-----------------------------");
+        System.out.println("Objective: The player must attempt to beat the dealer \nby getting a count as close to 21 as \n possible, without going over 21.");
+        System.out.println("- Player will determine whether the value of \nthe ace is 1 or 11. All other cards will \nhave the value of the cards are their pip value. \n Kings, Queens, and Jacks will all have a value of 10. ");
+        System.out.println("- At the start of each game, the player will place a bet of any value between $2 and $500.");
+    }
+    public static void printMenu(int points, int won){
+        //MENU
+        System.out.println("\tBlack Jack \n-----------------------");
+        System.out.println("Player Score: " + points + "\nGames won: " + won + "\nWallet: " + wallet);
+        System.out.println("1) Hit");
+        System.out.println("2) Stand");
+        System.out.println("3) Rules");
+
+    }
+    public static void hit(){
+        //will hit
+    }
+    public static void split(){
+        // will hit
+    }
+    public static void stand(){
+        //will stand
+    }
+    public static void printHand(LinkedList hand){
+        //will figure out later?
+
+    }
 }
 /*
  * this contains the main method that will be used in 
